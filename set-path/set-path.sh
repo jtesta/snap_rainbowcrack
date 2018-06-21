2462e54d99b3ea1a7df00c1a0685c5de77d467ca
Joe Testa <jtesta@positronsecurity.com>
2018-06-21 15:23:15 -0400

    Changed working directory path to sub-directory within user's home.

--- set-path/set-path.sh
+++ set-path/set-path.sh
@@ -2,15 +2,15 @@
 
 # Setting LD_LIBRARY_PATH doesn't work, since the binaries explicitly open this
 # library from the local directory.
-if [ ! -f $SNAP_COMMON/alglib0.so ]; then
-  cp $SNAP/alglib0.so $SNAP_COMMON
+if [ ! -f $SNAP_USER_COMMON/alglib0.so ]; then
+  cp $SNAP/alglib0.so $SNAP_USER_COMMON
 fi
 
-if [ ! -f $SNAP_COMMON/charset.txt ]; then
-  cp $SNAP/charset.txt $SNAP_COMMON
+if [ ! -f $SNAP_USER_COMMON/charset.txt ]; then
+  cp $SNAP/charset.txt $SNAP_USER_COMMON
 fi
 
-pushd $SNAP_COMMON > /dev/null
+pushd $SNAP_USER_COMMON > /dev/null
 echo "Setting working directory to: $PWD..."
 CMD=$SNAP/$1
 shift # Remove the first element in the $@ array, since that has the $1 arg.
